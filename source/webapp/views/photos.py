4cb1447c00224a70fd298f1107f8c7b181cceda6
Daniarbekov <daniarbekov@gmail.com>
2022-11-26 13:35:14 +0600

    Права доступа
    добавил в представления проверки прав пользователя

--- source/webapp/views/photos.py
+++ source/webapp/views/photos.py
@@ -2,9 +2,10 @@ from django.views.generic import  CreateView, DetailView, UpdateView, DeleteView
 from webapp.forms import PhotoForm
 from webapp.models import Photo
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import  UserPassesTestMixin, LoginRequiredMixin
 
 
-class PhotoCreate(CreateView):
+class PhotoCreate(LoginRequiredMixin,CreateView):
     template_name = 'add_photo.html'
     form_class = PhotoForm
     model = Photo
@@ -19,14 +20,20 @@ class PhotoView(DetailView):
     model = Photo
 
 
-class PhotoUpdateView(UpdateView):
+class PhotoUpdateView(UserPassesTestMixin, UpdateView):
     template_name = 'photo_update.html'
     form_class = PhotoForm
     model = Photo
     context_object_name = 'photo'
     
+    def test_func(self):
+        return self.get_object().author == self.request.user or self.request.user.has_perm('webapp.change_photo')
+    
 
-class PhotoDeleteView(DeleteView):
+class PhotoDeleteView(UserPassesTestMixin,DeleteView):
     template_name = 'photo_delete.html'
     model = Photo
     success_url = reverse_lazy('index')
+
+    def test_func(self):
+        return self.get_object().author == self.request.user or self.request.user.has_perm('webapp.delete_photo')
