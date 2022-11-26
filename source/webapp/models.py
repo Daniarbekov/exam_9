ab6137ce5d75122f2348314639240f1399a38e4c
Daniarbekov <daniarbekov@gmail.com>
2022-11-26 11:41:22 +0600

    добавил модель Photo

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+
+class Photo(models.Models):
+    image = models.ImageField(
+        verbose_name='Фотография', 
+        upload_to='products'
+    )
+    note = models.CharField(
+        verbose_name='подпись',
+        max_length=100,
+    )
+    author = models.ForeignKey(
+        verbose_name='Автор',
+        to=get_user_model(),
+        related_name='reviews',
+        on_delete=models.CASCADE
+        )
+    created_at = models.DateTimeField(
+        verbose_name='дата создания',
+        auto_now_add=True
+        )
